604d7d28e4c39a379e12fb0ecbff2216eed421fe
javasno <70279387+ashish9348@users.noreply.github.com>
2025-01-27 11:48:04 +0000

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,14 +191,14 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/67976f6a825083258e0b925c/1iijomf63';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67976f6a825083258e0b925c/1iijomf63';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
 // End of Tawk.to Live Chat
 
